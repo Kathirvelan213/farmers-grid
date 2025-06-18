7f021845ee7428fd3138ee6bfb6ac37be2639631
kathirvelan <kathirvelanj1@gmail.com>
2025-06-18 13:20:35 +0530

    got started with the login page

---
+++ web-portal/src/assets/LoginPage/LoginPage.jsx
@@ -0,0 +1,10 @@
+import { LoginBox } from "./components/LoginBox"
+
+export function LoginPage(){
+    return (
+        <div className='loginPage'>
+            <LoginBox></LoginBox>
+        </div>
+
+    )
+}
---
+++ web-portal/src/assets/LoginPage/components/LoginBox.jsx
@@ -0,0 +1,11 @@
+import "../styles/LoginPage.css"
+
+export function LoginBox(){
+    return (
+        <div className="loginBox">
+            <input className="inputFields"></input>
+            <input className="inputFields"></input>
+        </div>
+    )
+}
+
