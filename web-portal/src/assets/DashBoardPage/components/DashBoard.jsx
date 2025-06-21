9a133ae7dc78a4fb6b803db2f8c6eef918c49c09
kathirvelan <kathirvelanj1@gmail.com>
2025-06-21 21:08:52 +0530

    added the api consumer for products and used that data for searchpanel

--- web-portal/src/assets/DashBoardPage/components/DashBoard.jsx
+++ web-portal/src/assets/DashBoardPage/components/DashBoard.jsx
@@ -1,23 +1,43 @@
 import '../styles/DashBoardPage.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import { getProductsAPI } from '../../apiConsumer/productsAPI';
 
 export function SearchPanel(){
     const [selected,setSelected]=useState('');
+    const [products,setProducts]=useState([]);
+
+    useEffect(()=>{
+        const getProducts=async ()=>{
+            try{
+                const result=await getProductsAPI();
+                setProducts(result.data);
+                console.log(result.data);
+            }
+            catch(e){
+                console.error(e);
+            }
+        }
+        getProducts();
+    },[]);
+
+    
+    
     function handleSearchChange(e){
         setSelected(e.target.value);
     }
-    
-    const Products=[{"id":1,"name":"potato"},{"id":2,"name":'tomato'},{"id":3,"name":'spinach'},{"id":4,"name":'brinjal'}]
+    // const Products=[{"id":1,"name":"potato"},{"id":2,"name":'tomato'},{"id":3,"name":'spinach'},{"id":4,"name":'brinjal'}]
     const query=selected.toLowerCase(); 
+    const filteredProducts=products.filter((product)=>{
+                return query===''||product.name.toLowerCase().includes(query);
+            })
+            
     return (
         <div className='searchPanel'>
             <input placeholder='Enter' value={selected} onChange={handleSearchChange}/>
-            {Products.filter((product)=>{
-                return query===''||product.name.toLowerCase().includes(query);
-            }).map((filterdProduct,index)=>(
+            {filteredProducts.map((product,index)=>(
                 <div key={index}>
                     <label>
-                        {filterdProduct.name}
+                        {product.name}
                     </label>
                 </div>
             ))}
---
+++ web-portal/src/assets/apiConsumer/productsAPI.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+
+console.log(import.meta.env.VITE_API_URL);
+const axiosInstance=axios.create({
+    baseURL:import.meta.env.VITE_API_URL,
+    headers:{
+        "Content-Type":"application/json",
+    },
+    withCredentials:true
+})
+
+export const getProductsAPI=()=>axiosInstance.get('/api/products');   
