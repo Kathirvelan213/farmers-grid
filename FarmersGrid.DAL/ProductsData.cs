cad66ee740f0743e3fb950cd302e69112c944055
kathirvelan <kathirvelanj1@gmail.com>
2025-06-24 12:41:54 +0530

    added sellerProduct related sps in db and added the appropriate callers in DAL

--- FarmersGrid.DAL/ProductsData.cs
+++ FarmersGrid.DAL/ProductsData.cs
@@ -1,6 +1,7 @@
 ﻿
 using Dapper;
 using FarmersGrid.Models;
+using System.Data;
 
 namespace FarmersGrid.DAL
 
@@ -14,8 +15,31 @@ namespace FarmersGrid.DAL
         }
         public async Task<IEnumerable<Product>> GetProducts()
         {
-            DynamicParameters dynamicParameters = new DynamicParameters();
-            return await _dbService.QueryAsync<Product>("usp_GetProducts", dynamicParameters);
+            DynamicParameters parameters = new DynamicParameters();
+            return await _dbService.QueryAsync<Product>("usp_GetProducts", parameters);
+        }
+        public async Task<IEnumerable<Product>> GetSellerProducts(string userId)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@userId", userId);
+            return await _dbService.QueryAsync<Product>("usp_GetSellerProducts", parameters);
+        }
+        public async Task<int> InsertSellerProduct(string userId, int productId,float price)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@userId",userId);
+            parameters.Add("@productId",productId);
+            parameters.Add("@unitPrice",price);
+            parameters.Add("ReturnValue", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+
+            return await _dbService.ExecuteAsync("usp_InsertSellerProduct", parameters);
+
+        }
+        public async Task<int> DeleteSellerProduct(string id)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@id", id);
+            return await _dbService.ExecuteAsync("usp_DeleteSellerProducts", parameters);
         }
     }
 }
