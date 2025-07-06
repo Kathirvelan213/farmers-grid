d0362f571d1ef728350b95413d34a80d34cac9f4
kathirvelan <kathirvelanj1@gmail.com>
2025-07-06 22:38:42 +0530

    started adding the registration part

---
+++ web-portal/src/assets/LoginPage/components/RegisterPanels.jsx
@@ -0,0 +1,17 @@
+import '../styles/LoginPage.css';
+
+export function LeftRegisterPanel(){
+    return(
+        <div className='leftRegisterPanel'>
+left
+        </div>
+    )
+}
+
+export function RightRegisterPanel(){
+    return(
+        <div className='rightRegisterPanel'>
+right
+        </div>
+    )
+}
